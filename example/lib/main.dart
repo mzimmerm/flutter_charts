616a4fa8bbb4bd2d39d20b47eecba1b0ae17df1b
Milan Zimmermann <milan.zimmermann@gmail.com>
2021-06-14 15:07:53 -0400

    Mostly documentation of future refactorings.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -583,7 +583,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   // Row -> Expanded -> Chart expands chart horizontally <-->
                   new Expanded(
                     // #### Core chart
-                    child: lineChart, // verticalBarChart, lineChart
+                    child: verticalBarChart, // verticalBarChart, lineChart
                   ),
                   new Text('<<'), // labels fit horizontally
                   // new Text('<<<<<<'), // default, labels tilted, all present
