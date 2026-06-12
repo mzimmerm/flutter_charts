2702115cf7ae73bd70da2ed760f98903979ad5b9
Milan Zimmermann <milan.zimmermann@gmail.com>
2026-06-12 04:44:59 -0400

    start emulator change minor.

--- tool/test/start_emulator.sh
+++ tool/test/start_emulator.sh
@@ -4,7 +4,7 @@
 
   # This is the AVD emulator we request to exist
   # emulator_used="Nexus_6_API_35"
-  emulator_used="medium-phone" # "pixel-10-pro-xl" # "pixel-10-pro-xl" "emulator-5554"
+  emulator_used="medium-phone" # "medium-phone" # "pixel-10-pro-xl" "emulator-5554"
 
   echo Check if emulator is running
   if ! flutter emulators  2>/dev/null | grep --quiet "$emulator_used "; then
@@ -16,7 +16,8 @@
   # The only way to find out if the emulator is connected is to run ps, searching for the device name.
   # The potential alternative "flutter devices" lists only the short device name such as e3565.
   # todo-00-delete: if ! ps -alef | grep "$emulator_used" | grep -v grep ; then
-  if ! pgrep --ignore-case "$emulator_used"; then
+  # if ! pgrep --ignore-case "$emulator_used"; then
+  if ! ps -alef | grep "$emulator_used" | grep -v grep ; then
     echo No AVD devices running using the emulator $emulator_used. Launching the emulator.
     flutter emulators --launch "$emulator_used"
     emulator_wait_sleep=42 # 42 on laptop, 24 on server
