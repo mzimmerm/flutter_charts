22407a17f25054deade3aada6562961e989c3e6a
Milan Zimmermann <milan.zimmermann@gmail.com>
2026-07-31 16:38:22 -0400

    Changed configuration on server, everything runs there.
    Pushing to test on laptop.

--- tool/test/start_emulator.sh
+++ tool/test/start_emulator.sh
@@ -3,22 +3,23 @@
 #  - If Android AVD emulator is not running, starts one.
 
   # This is the AVD emulator we request to exist
-  # emulator_used="Nexus_6_API_35"
+  # emulators_tested="Nexus_6_API_35"
   emulator_used="medium-phone" # "medium-phone" # "pixel-10-pro-xl" "emulator-5554"
+  emulators_tested="medium-phone\|emulator-5554" # Sometimes, emulator_used is launched as emulator-5554
 
   echo Check if emulator is running
-  if ! flutter emulators  2>/dev/null | grep --quiet "$emulator_used "; then
-    echo "Emulator $emulator_used does not exist. Please create it, our integration tests depend on it. Exiting"
+  if ! flutter emulators  2>/dev/null | grep --quiet "$emulators_tested "; then
+    echo "No emulator in $emulators_tested exists. Please create one, our integration tests depend on it. Exiting"
     exit 1
   fi
 
   echo Check if the emulator named $emulator_used is connected to a running device.
   # The only way to find out if the emulator is connected is to run ps, searching for the device name.
   # The potential alternative "flutter devices" lists only the short device name such as e3565.
-  # todo-00-delete: if ! ps -alef | grep "$emulator_used" | grep -v grep ; then
-  # if ! pgrep --ignore-case "$emulator_used"; then # failes to find emulator. May need to change
-  if ! ps -alef | grep "$emulator_used" | grep -v grep ; then
-    echo No AVD devices running using the emulator $emulator_used. Launching the emulator.
+  # todo-00-delete: if ! ps -alef | grep "$emulators_tested" | grep -v grep ; then
+  # if ! pgrep --ignore-case "$emulators_tested"; then # failes to find emulator. May need to change
+  if ! ps -alef | grep "$emulators_tested" | grep -v grep ; then
+    echo No AVD devices running using the emulator $emulators_tested. Launching the emulator.
     flutter emulators --launch "$emulator_used"
     emulator_wait_sleep=42 # 42 on laptop, 24 on server
     echo Sleeping $emulator_wait_sleep.
