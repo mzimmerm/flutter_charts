2010ca67faf1cdaba6918398ad628d4f22551be2
Milan Zimmermann <milan.zimmermann@gmail.com>
2026-07-31 04:09:03 -0400

    Deleted old files config.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,15 +5,20 @@ allprojects {
     }
 }
 
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
+//rootProject.buildDir = '../build'
+
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+//    project.buildDir = "${rootProject.buildDir}/${project.name}"
 }
 subprojects {
     project.evaluationDependsOn(":app")
